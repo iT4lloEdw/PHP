52937de1751578721bfcbd123d8610557b842929
Itallo Edward <128102018+iT4lloEdw@users.noreply.github.com>
2023-05-12 16:28:14 -0300

    12/05 php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu Site</title>
-</head>
-<body>
-  <center>
-      <form id="login" action="index.php">
-          <input type="submit" name="Entrar" value="Entrar">
-   <center>       
-</body>
-</html>
+<?php
+for ($i=0; $i<10; $i++) {
+    echo $i;
+ }
+ ?>   
